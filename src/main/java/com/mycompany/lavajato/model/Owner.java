1c4249a4909317d71589ea76ba4e110d32626a0e
Lucas Alves <alvez.lucas2702@gmail.com>
2024-07-20 20:25:00 -0300

    feat: add model owner

---
+++ src/main/java/com/mycompany/lavajato/model/Owner.java
@@ -0,0 +1,33 @@
+package com.mycompany.lavajato.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "owners")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "phone_number",nullable = false)
+    private String phoneNumber;
+}
