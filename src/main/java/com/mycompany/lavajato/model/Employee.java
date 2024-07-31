5571f61732ab0c81294075032c5966b6e69e4c2a
Lucas Alves <alvez.lucas2702@gmail.com>
2024-07-30 22:12:48 -0300

    feat: add model attendance and employee

---
+++ src/main/java/com/mycompany/lavajato/model/Employee.java
@@ -0,0 +1,36 @@
+package com.mycompany.lavajato.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private Double commission;
+}
